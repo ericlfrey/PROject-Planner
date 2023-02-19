06b4fef6e23fb834f44786f1aac393f874818a04
Eric Frey <ericlfrey@gmail.com>
2023-02-19 16:03:29 -0600

    Used API call to get all Projects on Homepage

--- components/ProjectCard/ProjectCard.js
+++ components/ProjectCard/ProjectCard.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './ProjectCard.module.css';
 
-export default function ProjectCard() {
+export default function ProjectCard({ projectObj }) {
   return (
     <div>
       <h1 className={styles.cardHeading}>📒</h1>
-      <h5>Project Title</h5>
+      <h5>{projectObj.title}</h5>
     </div>
   );
 }
+
+ProjectCard.propTypes = {
+  projectObj: PropTypes.shape({
+    firebaseKey: PropTypes.string,
+    title: PropTypes.string,
+  }).isRequired,
+};
