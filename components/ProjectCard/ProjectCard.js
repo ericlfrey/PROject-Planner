f377a9c2a25245c83220f061ca900e3c87360248
Eric Frey <ericlfrey@gmail.com>
2023-02-20 17:35:36 -0600

    Added Link to the ProjectCard component to take the user to the project details page.

--- components/ProjectCard/ProjectCard.js
+++ components/ProjectCard/ProjectCard.js
@@ -1,16 +1,20 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
+import Link from 'next/link';
 import styles from './ProjectCard.module.css';
 
 export default function ProjectCard({ projectObj }) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title className={styles.cardHeading}>📒</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{projectObj.title}</Card.Subtitle>
-      </Card.Body>
-    </Card>
+    <Link passHref href={`./project/${projectObj.firebaseKey}`}>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title className={styles.cardHeading}>📒</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted">{projectObj.title}</Card.Subtitle>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 }
 
