487498a2202e6994e09e37527315b75b619d6750
Eric Frey <ericlfrey@gmail.com>
2023-02-20 19:20:28 -0600

    Created a basic Card for Tasks that are rendered on the Project Details Page.

---
+++ components/TaskCard/TaskCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+export default function TaskCard({ taskObj }) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{taskObj.task_name}</Card.Title>
+        <Card.Link href="#">View</Card.Link>
+        <Card.Link href="#">Edit</Card.Link>
+        <Card.Link href="#">Delete</Card.Link>
+      </Card.Body>
+    </Card>
+  );
+}
+
+TaskCard.propTypes = {
+  taskObj: PropTypes.shape({
+    firebaseKey: PropTypes.string,
+    project_id: PropTypes.string,
+    task_name: PropTypes.string,
+    details: PropTypes.string,
+    date_created: PropTypes.string,
+    due_date: PropTypes.string,
+    complete: PropTypes.bool,
+    in_progress: PropTypes.bool,
+  }).isRequired,
+};
