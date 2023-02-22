97f14a69405e1c55ed65773bf142853871b59504
Eric Frey <ericlfrey@gmail.com>
2023-02-21 21:03:44 -0600

    Added State to the EditTaskPage to get the single task. Added taslObj as a prop to the Task Form.

--- components/TaskForm/TaskForm.js
+++ components/TaskForm/TaskForm.js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { createTask, updateTask } from '../../api/taskData';
@@ -15,10 +15,14 @@ const initialState = {
   in_progress: false,
 };
 
-export default function TaskForm({ projectFirebaseKey }) {
+export default function TaskForm({ projectFirebaseKey, taskObj }) {
   const [formInput, setFormInput] = useState(initialState);
 
   const router = useRouter();
+
+  useEffect(() => {
+    if (taskObj.firebaseKey) setFormInput(taskObj);
+  }, [taskObj]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -81,8 +85,19 @@ export default function TaskForm({ projectFirebaseKey }) {
 
 TaskForm.propTypes = {
   projectFirebaseKey: PropTypes.string,
+  taskObj: PropTypes.shape({
+    firebaseKey: PropTypes.string,
+    project_id: PropTypes.string,
+    task_name: PropTypes.string,
+    details: PropTypes.string,
+    date_created: PropTypes.string,
+    due_date: PropTypes.string,
+    complete: PropTypes.bool,
+    in_progress: PropTypes.bool,
+  }),
 };
 
 TaskForm.defaultProps = {
   projectFirebaseKey: '',
+  taskObj: initialState,
 };
--- pages/task/edit/[firebaseKey].js
+++ pages/task/edit/[firebaseKey].js
@@ -1,14 +1,24 @@
 import Head from 'next/head';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { getSingleTask } from '../../../api/taskData';
 import TaskForm from '../../../components/TaskForm/TaskForm';
 
 export default function EditTaskPage() {
+  const [task, setTask] = useState({});
+
+  const router = useRouter();
+  const { firebaseKey } = router.query;
+
+  useEffect(() => {
+    getSingleTask(firebaseKey).then(setTask);
+  }, [firebaseKey]);
   return (
     <>
       <Head>
         <title>Edit Dynamic Task</title>
       </Head>
-      <TaskForm />
+      <TaskForm taskObj={task} />
     </>
   );
 }
