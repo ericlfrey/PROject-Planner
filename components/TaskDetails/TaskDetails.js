d89b3bf4aaa8ae31897242a07f5d3d59b44ce8ab
Eric Frey <ericlfrey@gmail.com>
2023-02-22 16:39:44 -0600

    Added delete functionality to the Task Details Page. After Deleting, the User is routed back to the Project Details Page.

--- components/TaskDetails/TaskDetails.js
+++ components/TaskDetails/TaskDetails.js
@@ -2,17 +2,27 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 import Link from 'next/link';
-import { getSingleTask } from '../../api/taskData';
+import { useRouter } from 'next/router';
+import { deleteTask, getSingleTask } from '../../api/taskData';
 import { getSingleProject } from '../../api/projectData';
 
 export default function TaskDetails({ firebaseKey }) {
   const [task, setTask] = useState({});
   const [project, setProject] = useState({});
 
+  const router = useRouter();
+
   useEffect(() => {
     getSingleTask(firebaseKey).then(setTask);
     if (task.project_id) getSingleProject(task.project_id).then(setProject);
   }, [firebaseKey, task.project_id]);
+
+  const handleDeleteTask = () => {
+    if (window.confirm(`Are you sure you want to delete "${task.task_name}"? This task cannot be undone.`)) {
+      deleteTask(firebaseKey).then(router.push(`/project/${project.firebaseKey}`));
+    }
+  };
+
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
@@ -24,7 +34,7 @@ export default function TaskDetails({ firebaseKey }) {
         <Link passHref href={`/task/edit/${firebaseKey}`}>
           <Card.Link href="#">Edit</Card.Link>
         </Link>
-        <Card.Link href="#">Delete</Card.Link>
+        <Card.Link href="#" onClick={handleDeleteTask}>Delete</Card.Link>
         <Link passHref href={`/project/${task.project_id}`}>
           <Card.Link href="#">Go Back</Card.Link>
         </Link>
