27964ed8576e5e5a3db3a7aa03ff043f59c01a8b
Eric Frey <ericlfrey@gmail.com>
2023-02-21 19:58:20 -0600

    Added dynamic Head title.

--- pages/task/new/[projectFirebaseKey].js
+++ pages/task/new/[projectFirebaseKey].js
@@ -1,15 +1,22 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getSingleProject } from '../../../api/projectData';
 import TaskForm from '../../../components/TaskForm/TaskForm';
 
 export default function AddTaskPage() {
+  const [project, setProject] = useState({});
+
   const router = useRouter();
   const { projectFirebaseKey } = router.query;
+
+  useEffect(() => {
+    getSingleProject(projectFirebaseKey).then(setProject);
+  }, [projectFirebaseKey]);
   return (
     <>
       <Head>
-        <title>Add New Task</title>
+        <title>Add Task to {project.title}</title>
       </Head>
       <TaskForm projectFirebaseKey={projectFirebaseKey} />
     </>
