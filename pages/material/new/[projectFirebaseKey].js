63a55b11b248d933596323c62f9a4579cf216342
Eric Frey <ericlfrey@gmail.com>
2023-02-24 15:43:11 -0600

    Form is on page, I still need to add task select.

--- pages/material/new/[projectFirebaseKey].js
+++ pages/material/new/[projectFirebaseKey].js
@@ -1,13 +1,25 @@
 import Head from 'next/head';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { getSingleProject } from '../../../api/projectData';
+import MaterialForm from '../../../components/MaterialForm/MaterialForm';
 
 export default function AddMaterialPage() {
+  const [project, setProject] = useState({});
+
+  const router = useRouter();
+  const { projectFirebaseKey } = router.query;
+
+  useEffect(() => {
+    getSingleProject(projectFirebaseKey).then(setProject);
+  }, [projectFirebaseKey]);
+
   return (
     <>
       <Head>
-        <title>Add New Material</title>
+        <title>Add Material to {project.title}</title>
       </Head>
-      <h1>AddMaterialPage</h1>
+      <MaterialForm projectFirebaseKey={projectFirebaseKey} />
     </>
   );
 }
