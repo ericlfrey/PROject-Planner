8ee7ef89e968efeead18b37b34a819f3d946eca9
Eric Frey <ericlfrey@gmail.com>
2023-02-25 11:52:41 -0600

    Added very simple Search Bar form. Need to make it functional.

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import {
+  Button, Form, FormControl, InputGroup,
+} from 'react-bootstrap';
 
 export default function SearchBar() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>SearchBar</div>
+    <Form onSubmit={handleSubmit}>
+      <InputGroup className="m-auto">
+        <FormControl
+          // className={styles.searchInput}
+          placeholder="Search"
+          aria-label="Search"
+          aria-describedby="basic-addon2"
+        // onChange={handleChange}
+        // value={searchInput}
+        />
+        <Button type="submit">
+          search
+        </Button>
+      </InputGroup>
+    </Form>
   );
 }
