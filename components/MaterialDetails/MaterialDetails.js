e97048ea30f56007f1031380d1b7974b780f2612
Eric Frey <ericlfrey@gmail.com>
2023-02-25 09:39:39 -0600

    Added Links to the MaterialCard and MaterialDetails components to take the user to the EditMaterialPage.

--- components/MaterialDetails/MaterialDetails.js
+++ components/MaterialDetails/MaterialDetails.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-// import Link from 'next/link';
+import Link from 'next/link';
 import { getSingleMaterial } from '../../api/materialData';
 import { getSingleProject } from '../../api/projectData';
 
@@ -26,9 +26,9 @@ export default function MaterialDetails({ firebaseKey }) {
         <Card.Subtitle className="mb-2 text-muted">Price: {material.price}</Card.Subtitle>
         <Card.Subtitle className="mb-2 text-muted">Quantity: {material.quantity}</Card.Subtitle>
 
-        {/* <Link passHref href={`/task/edit/${firebaseKey}`}> */}
-        <Card.Link href="#">Edit</Card.Link>
-        {/* </Link> */}
+        <Link passHref href={`/material/edit/${firebaseKey}`}>
+          <Card.Link href="#">Edit</Card.Link>
+        </Link>
         <Card.Link href="#" onClick={handleDeleteTask}>Delete</Card.Link>
         {/* <Link passHref href={`/project/${task.project_id}`}> */}
         <Card.Link href="#">Go Back</Card.Link>
