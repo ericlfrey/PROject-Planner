508ea0e9862632f94caecb49ff22036d2ecc446c
Eric Frey <ericlfrey@gmail.com>
2023-02-25 12:07:53 -0600

    Search Bar routes to the SearchPage. Need to add functionality to the SearchPage.

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 import {
   Button, Form, FormControl, InputGroup,
 } from 'react-bootstrap';
 
 export default function SearchBar() {
+  const [searchInput, setSearchInput] = useState('');
+  const router = useRouter();
+
+  const handleChange = (e) => {
+    setSearchInput(e.target.value.toLowerCase());
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (searchInput !== '') router.push(`/search/${searchInput}`);
+    setSearchInput('');
   };
   return (
     <Form onSubmit={handleSubmit}>
       <InputGroup className="m-auto">
         <FormControl
-          // className={styles.searchInput}
           placeholder="Search"
-          aria-label="Search"
-          aria-describedby="basic-addon2"
-        // onChange={handleChange}
-        // value={searchInput}
+          onChange={handleChange}
+          value={searchInput}
         />
         <Button type="submit">
           search
