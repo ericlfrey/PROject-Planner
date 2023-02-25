6e14ed752e44bd6ff1ffcbaa0c33dff705cd4800
Eric Frey <ericlfrey@gmail.com>
2023-02-25 11:34:32 -0600

    Delete Material works on the MaterialCard component.

--- components/MaterialCard/MaterialCard.js
+++ components/MaterialCard/MaterialCard.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import { deleteMaterial } from '../../api/materialData';
 
-export default function MaterialCard({ materialObj }) {
+export default function MaterialCard({ materialObj, onChange }) {
+  const handleDeleteMaterial = () => {
+    if (window.confirm(`Are you sure you want to delete "${materialObj.material_name}"? This task cannot be undone.`)) {
+      deleteMaterial(materialObj.firebaseKey).then(onChange);
+    }
+  };
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Body>
@@ -14,7 +20,7 @@ export default function MaterialCard({ materialObj }) {
         <Link passHref href={`/material/edit/${materialObj.firebaseKey}`}>
           <Card.Link href="#">Edit</Card.Link>
         </Link>
-        <Card.Link href="#">Delete</Card.Link>
+        <Card.Link href="#" onClick={handleDeleteMaterial}>Delete</Card.Link>
       </Card.Body>
     </Card>
   );
@@ -30,4 +36,5 @@ MaterialCard.propTypes = {
     quantity: PropTypes.string,
     acquired: PropTypes.bool,
   }).isRequired,
+  onChange: PropTypes.func.isRequired,
 };
