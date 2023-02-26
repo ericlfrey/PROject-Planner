bbbb0dda49b1a7bae360981de235b0e1b0af7f1b
Eric Frey <ericlfrey@gmail.com>
2023-02-26 14:58:10 -0600

    removed the pseudo class for now becuase it was causing problems. Will circle back to this. Created a basic logo. Continuing to style the navbar.

--- components/NavBar/NavBar.js
+++ components/NavBar/NavBar.js
@@ -1,17 +1,20 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { signOut } from '../../utils/auth';
 import SearchBar from '../SearchBar/SearchBar';
 import styles from './NavBar.module.css';
+import logo from '../../public/images/logo.png';
 
 export default function NavBar() {
   return (
     <nav className={`navbar navbar-expand-md ${styles.navbar}`}>
-      <div className="container-fluid">
+      <div className={`container-fluid ${styles.navContainer}`}>
         <Link passHref href="/">
-          <a className="navbar-brand">
-            📒 PROject Planner
+          <a className={`navbar-brand ${styles.navbarBrand}`}>
+            <Image src={logo} height={60} width={60} />
+            {/* 📒 PROject Planner */}
           </a>
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,8 +22,8 @@ export default function NavBar() {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <ul className="navbar-nav me-auto">
-            <div className="nav-links">
+          <ul className={`navbar-nav me-auto ${styles.navbarNav}`}>
+            <div className={`nav-links ${styles.navLinks}`}>
               <li className="nav-item">
                 <Link passHref href="/">
                   <a className="nav-link">
