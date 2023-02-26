34e898c337bcfe4ecd6ded3cdb9ac8ec3228fd99
Eric Frey <ericlfrey@gmail.com>
2023-02-26 11:26:29 -0600

    Added toFixed(2) to the totalCost on the ProjectDetails component so that it only displays 2 decimal places.

--- components/ProjectDetails/ProjectDetails.js
+++ components/ProjectDetails/ProjectDetails.js
@@ -16,7 +16,7 @@ export default function ProjectDetails({ project }) {
   useEffect(() => {
     getProjectMaterials(project.firebaseKey).then(setMaterials);
   }, [project]);
-  const totalCost = materials.length > 0 ? materials.map((material) => material.price * material.quantity).reduce((a, b) => a + b) : '0';
+  const totalCost = materials.length > 0 ? materials.map((material) => material.price * material.quantity).reduce((a, b) => a + b).toFixed(2) : '0';
 
   const handleDelete = () => {
     if (window.confirm('Are you sure you want to delete the Project?')) {
