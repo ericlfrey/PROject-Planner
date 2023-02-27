ca21538ccf475394f2b41ff5797298ee74bedf70
Eric Frey <ericlfrey@gmail.com>
2023-02-27 14:27:15 -0600

    Added module css file for the Home Page.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable react-hooks/exhaustive-deps */
 import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import { getUserProjects } from '../api/projectData';
 import ProjectCard from '../components/ProjectCard/ProjectCard';
 import { useAuth } from '../utils/context/authContext';
+import styles from '../styles/Home.module.css';
 
 function Home() {
   const [projects, setProjects] = useState([]);
@@ -18,6 +20,7 @@ function Home() {
       <Head>
         <title>Home</title>
       </Head>
+      <h4 className={styles.heading}>{user.displayName}'s Current Open Projects:</h4>
       <div className="d-flex flex-wrap justify-content-center">
         {projects.map((project) => (
           <ProjectCard
