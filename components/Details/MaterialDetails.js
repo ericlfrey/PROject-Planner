90a4d79e170d301143fae9ca5b159c09d27ffb42
Eric Frey <ericlfrey@gmail.com>
2023-02-28 15:37:33 -0600

    Material Details Page Looks Right. Might come back to change info on page.

--- components/Details/MaterialDetails.js
+++ components/Details/MaterialDetails.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Dropdown } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { deleteMaterial, getSingleMaterial } from '../../api/materialData';
 import { getSingleProject } from '../../api/projectData';
+import cardStyles from '../../styles/CardStyles.module.css';
+import { getSingleTask } from '../../api/taskData';
 
 export default function MaterialDetails({ firebaseKey }) {
   const [material, setMaterial] = useState({});
   const [project, setProject] = useState({});
+  const [task, setTask] = useState({});
 
   const router = useRouter();
 
   useEffect(() => {
     getSingleMaterial(firebaseKey).then(setMaterial);
     if (material.project_id) getSingleProject(material.project_id).then(setProject);
-  }, [firebaseKey, material.project_id]);
+    if (material.task_id) getSingleTask(material.task_id).then(setTask);
+  }, [firebaseKey, material.project_id, material.task_id]);
 
   const handleDeleteMaterial = () => {
     if (window.confirm(`Are you sure you want to delete "${material.material_name}"? This task cannot be undone.`)) {
@@ -24,20 +28,35 @@ export default function MaterialDetails({ firebaseKey }) {
   };
 
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card className={cardStyles.topCard}>
       <Card.Body>
-        <Card.Title>{material.material_name}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Project: {project.title}</Card.Subtitle>
-        <Card.Subtitle className="mb-2 text-muted">{material.acquired ? 'Acquired' : 'Not Acquired'}</Card.Subtitle>
-        <Card.Subtitle className="mb-2 text-muted">Price: {material.price}</Card.Subtitle>
-        <Card.Subtitle className="mb-2 text-muted">Quantity: {material.quantity}</Card.Subtitle>
-        <Link passHref href={`/material/edit/${firebaseKey}`}>
-          <Card.Link href="#">Edit</Card.Link>
-        </Link>
-        <Card.Link href="#" onClick={handleDeleteMaterial}>Delete</Card.Link>
-        <Link passHref href={`/project/${material.project_id}`}>
-          <Card.Link href="#">Go Back</Card.Link>
-        </Link>
+        <blockquote className="blockquote mb-0">
+          <div className={cardStyles.cardHeader}>
+            <h3>{material.material_name}</h3>
+            <Dropdown>
+              <Dropdown.Toggle variant="outline-success" className={cardStyles.cardActionsBtn}>
+                Actions
+              </Dropdown.Toggle>
+              <Dropdown.Menu className={cardStyles.dropdownMenu}>
+                <Link passHref href={`/material/edit/${firebaseKey}`}>
+                  <Dropdown.Item className={cardStyles.dropdownItem}>Edit</Dropdown.Item>
+                </Link>
+                <Dropdown.Item className={cardStyles.dropdownItem} onClick={handleDeleteMaterial}>Delete Task
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+          <h5>Total Material Cost: ${(material.price * material.quantity).toFixed(2)}</h5>
+          <footer className={`${cardStyles.cardSubtitle} blockquote-footer mt-2`}>Project: {project.title}</footer>
+          <footer className={`${cardStyles.cardSubtitle} blockquote-footer mt-2`}>Task: {task.task_name}</footer>
+          <hr />
+          <Card.Text className={`${cardStyles.cardText} mt-2`}>Status: {material.acquired ? 'Acquired' : 'Not Acquired'}</Card.Text>
+          <Card.Text className={`${cardStyles.cardText} mt-2`}>Price: ${material.price}</Card.Text>
+          <Card.Text className={`${cardStyles.cardText} mt-2`}>Quantity: {material.quantity}</Card.Text>
+          <Link passHref href={`/project/${material.project_id}`}>
+            <Card.Link className={cardStyles.cardLink}> ← Go Back</Card.Link>
+          </Link>
+        </blockquote>
       </Card.Body>
     </Card>
   );
