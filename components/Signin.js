a702baac0911f9b1ee901dab968c697dd260335c
Eric Frey <ericlfrey@gmail.com>
2023-02-28 21:40:40 -0600

    Simple styling on the sign in page.

--- components/Signin.js
+++ components/Signin.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import Image from 'next/image';
 import { signIn } from '../utils/auth';
+import logo from '../public/images/logo.png';
 
 function Signin() {
   return (
     <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
+      className="signin-div"
+    // style={{
+    //   height: '90vh',
+    //   padding: '30px',
+    //   maxWidth: '400px',
+    //   margin: '0 auto',
+    // }}
     >
-      <h1>Hi there!</h1>
-      <p>Click the button below to login!</p>
-      <button type="button" className="btn btn-primary btn-lg copy-btn" onClick={signIn}>
+      <Image src={logo} className="logo" />
+      <button type="button" className="signin-btn" onClick={signIn}>
         Sign In
       </button>
     </div>
